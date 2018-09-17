2ccc967abd00a49d9e770b78bf93887941f74e4f
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-17 00:23:48 -0500

    keep moving forward always

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
 const logger = require("./logger.js");
 const path = require('path');
-
+const os = require('os');
 logger("Treek");
 
 var pathObj = path.parse(__filename);
 
 console.log(pathObj);
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(totalMemory);
+console.log(freeMemory);
