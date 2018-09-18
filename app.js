cfd60d461fe7c8045edbdcc625fdab68e5701d57
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-17 22:47:34 -0500

    keep moving forward

--- app.js
+++ app.js
@@ -8,27 +8,7 @@ const EventEmitter = require('events');
 // building a web server http
 
 // create an instance of the class
-// this emitter is an object
-const emitter = new EventEmitter();
 
-//Register a listener
-emitter.on('messageLogged',function(arg){
- console.log('Listner called',arg);
-});
+const http = require("http");
 
-// raises an event now you  a listener
-emitter.emit('messageLogged',{id:1,url: 'http://'});
-//raise an event =emitter.emit
-// now you need to call a listener that listens to the events.
-//emitter.emit ('messageLogged');   emit means =actually making a noise,produce - signnalling event is happening
-// passing an argument is the name of the event ,which is messaged logged
-
-emitter.on('Logging', function(arg) {
-    console.log('Treek you are an software engineer', arg );
-});
-// creagting an event called loggin and giving an id and data
-emitter.emit('Logging' , {id:2, url:'http://'});
-const files = fs.readdirSync('./',function(err,files){
-    if(err) console.log('Error',err);
-    else console.log('Result', files);
-});
+http.createServer();
--- logger.js
+++ logger.js
@@ -1,17 +1,35 @@
 
+// is a class for the events and extending the events
+const EventEmitter = require('events');
+
+// creates an instance which an object
+
+
 
     console.log(__filename);
     console.log(__dirname);
 
     var url = 'http://mylogger.io/log';
 
+// creating an constructor function to have all the capablities of the EventEmitter or module that is required use the extend
+    class Logger  extends EventEmitter {
+        // when the function inside a class... it become a function
+        log(message) {
+            // Send an Http Request
+            console.log(message);
+            // raises an event now you  a listener
+        this.emit('messageLogged',{id:1,url: 'http://'});
+        }
+    }
 
 function log(message) {
     // Send an Http Request
     console.log(message);
+    // raises an event now you  a listener
+emitter.emit('messageLogged',{id:1,url: 'http://'});
 }
 
 
-module.exports = log;
+module.exports = Logger;
 
     
