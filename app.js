97fc24146e62f038aa6e15629174675968679309
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-17 20:44:00 -0500

    keep moving forward

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const logger = require("./logger.js");
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+// calls out the eventEmitter class
+const EventEmitter = require('events');
+// building a web server http
 
-logger("Treek");
-
-var pathObj = path.parse(__filename);
-
-console.log(pathObj);
-
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
 
 const files = fs.readdirSync('./',function(err,files){
     if(err) console.log('Error',err);
     else console.log('Result', files);
 });
-console.log(files);
-
-console.log('Total Memory: ${totalMemory}')
