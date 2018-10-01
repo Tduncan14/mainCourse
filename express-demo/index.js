1e4a8224fe05eae3f7e6375f79b1deb73aa4537a
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-30 21:31:00 -0500

    keep moving forward always

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,8 +3,18 @@ const express = require('express');
  // storing the express instance in a variable
 const app=express();
 
-// http post request and endpoint
+// http post request and endpoint gives out a listener
+// needs a route location then a call back function the event handler//does things
 app.get("/",function(req,res) {
-    
+
+    res.send('Hello World!!!');
 
 });
+// creating another route
+app.get('/api/courses', function(req,res) {
+    res.send([1,2,3]);
+})
+// now to create the app listener
+// for hosting sites we need hosting variable PORT
+ const PORT = process.env.PORT || 3000;
+app.listen(PORT,() => console.log("Listening on port 3000"));
